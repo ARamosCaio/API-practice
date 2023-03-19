3d70641560711d0e9e2c3575a146bc33eedd1878
Caio Vinícius Araújo Ramos <aramoscaio@gmail.com>
2023-03-18 21:56:21 -0300

    Added authorization to the resources file to allow requests

--- Notes-API/notable_django/notable_django/settings.py
+++ Notes-API/notable_django/notable_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qjd171nuvwom@(mtxc8#ju9-=p5u5(#*b94)fni&i4@0u^f3k^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
