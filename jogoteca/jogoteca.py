76358157683f461ec9a6b43c55a34fdf7fdc8da5
Caio Ramos <aramoscaio@gmail.com>
2023-03-22 01:01:38 -0300

    Created the app Jogoteca and the templates folder

---
+++ jogoteca/jogoteca.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+class game:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+
+app = Flask(__name__)
+
+@app.route('/home')
+
+def greeting():
+    game1 = game('Tetris', 'Puzzle', 'Atari')
+    game2 = game('God of War', 'Rack n Slash', 'PS2')
+    game3 = game('Mortal Kombat', 'Luta', 'PS2')
+    game_list = [game1, game2, game3]
+    return render_template('game-list.html', title='Jogos', games = game_list)
+
+
+app.run()
