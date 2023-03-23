48cee93b0cc296c30373a4b959031b1c74258eab
Caio Ramos <aramoscaio@gmail.com>
2023-03-23 00:43:32 -0300

    Added the creation form interaction, now the game registered is saved showed on the list

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,28 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
-class game:
+class Game:
     def __init__(self, name, category, console):
         self.name = name
         self.category = category
         self.console = console
 
+game1 = Game('Tetris', 'Puzzle', 'Atari')
+game2 = Game('God of War', 'Rack n Slash', 'PS2')
+game3 = Game('Mortal Kombat', 'Luta', 'PS2')
+game_list = [game1, game2, game3]
+
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 
-def greeting():
-    game1 = game('Tetris', 'Puzzle', 'Atari')
-    game2 = game('God of War', 'Rack n Slash', 'PS2')
-    game3 = game('Mortal Kombat', 'Luta', 'PS2')
-    game_list = [game1, game2, game3]
+
+def index():
+   
     return render_template('game-list.html', title='Jogos', games = game_list)
 
 @app.route('/newgame')
 
-def new_game ():
-    return render_template("new_game.html", title='Novo Jogo')
+def new_game():
+    return render_template("new-game.html", title='Novo Jogo')
+
+@app.route("/create", methods=['POST',])
+
+def create():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Game(nome, categoria, console)
+    game_list.append(jogo)
+    return render_template('game-list.html', title='Jogo', games = game_list)
 
 
-app.run()
+app.run(debug=True)
